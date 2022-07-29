424a5bf968f4198963a83bb59cad81577ad436bc
StandingPad <sheikhmahid1@gmail.com>
2022-07-29 12:41:45 -0500

    Changed to MIT license

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,18 @@
 /*!
 # Resurgence
-
 A VM backend library that makes developing interpreters easy. Can be used either as an entire backend, or to create a backend
 */
 
+/*
+    Copyright (c) 2022 StandingPad
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
 pub(crate) mod objects;
 pub(crate) use objects::constant;
 pub(crate) mod internal;
