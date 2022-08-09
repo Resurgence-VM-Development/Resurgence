c594de916ac13304299430eb9a536e1c3466f4f8
StandingPad <sheikhmahid1@gmail.com>
2022-08-09 17:44:38 -0500

    Reformated the math operations

--- src/internal/interpreter/execution_engine.rs
+++ src/internal/interpreter/execution_engine.rs
@@ -42,18 +42,10 @@ impl ExecutionEngine for Interpreter {
                     self.stack.pop();
                 } // We have the braces around this call to make the Rust compiler happy
 
-                Instruction::Add(ref dst_reg, ref reg_1, ref reg_2) => {
-                    self.add(dst_reg, reg_1, reg_2)
-                }
-                Instruction::Sub(ref dst_reg, ref reg_1, ref reg_2) => {
-                    self.sub(dst_reg, reg_1, reg_2)
-                }
-                Instruction::Mul(ref dst_reg, ref reg_1, ref reg_2) => {
-                    self.mul(dst_reg, reg_1, reg_2)
-                }
-                Instruction::Div(ref dst_reg, ref reg_1, ref reg_2) => {
-                    self.div(dst_reg, reg_1, reg_2)
-                }
+                Instruction::Add(ref dst_reg, ref reg_1, ref reg_2) => self.add(dst_reg, reg_1, reg_2),
+                Instruction::Sub(ref dst_reg, ref reg_1, ref reg_2) => self.sub(dst_reg, reg_1, reg_2),
+                Instruction::Mul(ref dst_reg, ref reg_1, ref reg_2) => self.mul(dst_reg, reg_1, reg_2),
+                Instruction::Div(ref dst_reg, ref reg_1, ref reg_2) => self.div(dst_reg, reg_1, reg_2),
 
                 Instruction::Equal(ref reg_1, ref reg_2) => {
                     if self.equal(reg_1, reg_2) {
