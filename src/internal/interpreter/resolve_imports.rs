540605eec445b3b2fc5cbaf0d6c2f442161b2b22
dynafide <74845094+dynafide@users.noreply.github.com>
2022-08-22 10:46:51 -0500

    Fix bug in import resolver

--- src/internal/interpreter/resolve_imports.rs
+++ src/internal/interpreter/resolve_imports.rs
@@ -1,22 +1,24 @@
-use std::io::{Error, ErrorKind};
 use super::Interpreter;
+use std::io::{Error, ErrorKind};
 
 impl Interpreter {
     /// Resolves any Rust functions used in the bytecode file by creating a "compatibility layer" based on indicies
     pub fn resolve_imports(&mut self) -> Result<(), Error> {
         let imports = &self.code_holder.imports;
         for (index, name) in imports.iter().enumerate() {
-            if self.rust_functions[index].name == *name {
-                self.byte_to_interal[index] = index as u64;
-            }
-            else {
-                for (internal_index, internal_name) in self.rust_functions.iter().enumerate() {
-                    if *internal_name.name == *name {
-                        self.byte_to_interal[index] = internal_index as u64;
-                        return Result::Ok(());
-                    }
+            let mut success = false;
+            for (internal_index, internal_name) in self.rust_functions.iter().enumerate() {
+                if *internal_name.name == *name {
+                    self.byte_to_interal.insert(index, internal_index as u64);
+                    success = true;
                 }
-                return Err(Error::new(ErrorKind::NotFound, format!("Can not find function '{}' internally!", *name)))
+            }
+            if !success {
+                // Failed to find a matching import
+                return Err(Error::new(
+                    ErrorKind::NotFound,
+                    format!("Can not find function '{}' internally!", *name),
+                ));
             }
         }
         Result::Ok(())
