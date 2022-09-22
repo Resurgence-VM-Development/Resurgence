ab9f014ad3d5308bb649c612109c40ebd5e403a8
StandingPad Animations <sheikhmahid1@gmail.com>
2022-09-22 18:52:00 -0500

    Added the less than instruction (which seemed to not be added due to an oversignt)

--- src/internal/interpreter/instruction/cmp.rs
+++ src/internal/interpreter/instruction/cmp.rs
@@ -59,13 +59,14 @@ impl Interpreter {
     pub fn less_than(&mut self, reg_1: &Register, reg_2: &Register) -> bool {
         let (const_1, const_2) = self.get_constants(reg_1, reg_2);
         match (const_1, const_2) {
-            (Constant::Int(_), Constant::Int(_)) => todo!(),
-            (Constant::Int(_), Constant::Double(_)) => todo!(),
-            (Constant::Double(_), Constant::Int(_)) => todo!(),
-            (Constant::Double(_), Constant::Double(_)) => todo!(),
+            (Constant::Int(val_1), Constant::Int(val_2)) => val_1 < val_2,
+            (Constant::Int(val_1), Constant::Double(val_2)) => val_1 < val_2 as i64,
+            (Constant::Double(val_1), Constant::Int(val_2)) => val_1 as i64 < val_2,
+            (Constant::Double(val_1), Constant::Double(val_2)) => val_1 < val_2,
             _ => panic!("Invalid comparison!")
         }
     }
+
 
     /// Checks if a register is greater than or equal to another
     /// 
