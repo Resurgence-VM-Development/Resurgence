7c058e13e4fe43e5bfbbb379e61daf777a1aeebb
StandingPad Animations <sheikhmahid1@gmail.com>
2022-10-21 21:54:28 -0500

    byte_to_internal now preallocates memory

--- src/internal/interpreter/resolve_imports.rs
+++ src/internal/interpreter/resolve_imports.rs
@@ -5,6 +5,7 @@ impl Interpreter {
     /// Resolves any Rust functions used in the bytecode file by creating a "compatibility layer" based on indicies
     pub fn resolve_imports(&mut self) -> Result<(), Error> {
         let imports = &self.code_holder.imports;
+        self.byte_to_interal.reserve(imports.len()); // We know the amount of functions being used, so let's take advantage of that
         for (_, name) in imports.iter().enumerate() {
             let mut success = false;
             for (internal_index, internal_name) in self.rust_functions.iter().enumerate() {
