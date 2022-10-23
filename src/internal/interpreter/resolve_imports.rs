ce337bdff2c988c291c1b297509ab0cf20d384af
StandingPad Animations <sheikhmahid1@gmail.com>
2022-10-23 15:50:02 -0500

    Added cold attribute since it's only called once during runtime

--- src/internal/interpreter/resolve_imports.rs
+++ src/internal/interpreter/resolve_imports.rs
@@ -3,6 +3,7 @@ use std::io::{Error, ErrorKind};
 
 impl Interpreter {
     /// Resolves any Rust functions used in the bytecode file by creating a "compatibility layer" based on indicies
+    #[cold]
     pub fn resolve_imports(&mut self) -> Result<(), Error> {
         let imports = &self.code_holder.imports;
         self.byte_to_interal.reserve(imports.len()); // We know the amount of functions being used, so let's take advantage of that
