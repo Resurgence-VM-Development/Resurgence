98a6aaafa4f99694ef6b3b7d27558e4158c2d2e0
StandingPad Animations <sheikhmahid1@gmail.com>
2022-10-30 16:12:46 -0500

    began rewriting the codegen api

--- src/api/level_1/mod.rs
+++ src/api/level_1/mod.rs
@@ -1,15 +1,5 @@
-use std::{collections::HashMap};
-use crate::objects::codeholder::CodeHolder;
-mod generate_instruction;
+use crate::{CodeHolder, objects::instruction::Instruction};
 
-/// `CodeBuilder<'a>`: Helps contruct bytecode to be used by the VM
-struct CodeBuilder<'a> {
-    /// Reference to a `CodeHolder` 
-    code_holder: &'a mut CodeHolder,
-
-    /// Holds function symbols 
-    func_symbols: HashMap<String, usize>,
-    
-    /// Holds variable symbols
-    var_symbols: Vec<HashMap<String, u32>>
+pub fn generate_alloc(holder: &mut CodeHolder, amount: u32) {
+    holder.instructions.push(Some(Instruction::Alloc(amount)));
 }
