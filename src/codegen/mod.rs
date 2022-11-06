27acc7bb02dc0706da96fa02504b7bd5f2d8b129
StandingPad Animations <sheikhmahid1@gmail.com>
2022-11-06 11:20:46 -0600

    Fixed some codegen functions, added some abstraction for registers, and made some helper functions to convert from abstractions to real internals

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -1,8 +1,33 @@
-use crate::{CodeHolder, objects::{instruction::Instruction, register::RegisterLocation}};
+use crate::{CodeHolder, objects::{instruction::Instruction, register::{RegisterLocation, RegisterReference, Register}}};
 
 pub enum RVMLocation {
     GLOBAL,
     LOCAL
+}
+
+pub enum RVMReference {
+    AsIs,
+    DEREFERENCE
+}
+
+pub struct RVMRegister(u32, RVMLocation);
+
+fn real_loc(moc_loc: RVMLocation) -> RegisterLocation {
+    match moc_loc {
+        RVMLocation::GLOBAL => RegisterLocation::Global,
+        RVMLocation::LOCAL => RegisterLocation::Local
+    }
+}
+
+fn real_reference(moc_ref: RVMReference) -> RegisterReference {
+    match moc_ref {
+        RVMReference::AsIs => RegisterReference::AsIs,
+        RVMReference::DEREFERENCE => RegisterReference::Dereference
+    }
+}
+
+fn real_register(moc_reg: RVMRegister) -> Register {
+    Register(moc_reg.0, real_loc(moc_reg.1))
 }
 
 pub fn generate_alloc(holder: &mut CodeHolder, amount: u32) {
@@ -14,31 +39,33 @@ pub fn generate_free(holder: &mut CodeHolder, amount: u32) {
 }
 
 pub fn generate_frame_alloc(holder: &mut CodeHolder, amount: u32, location: RVMLocation) {
-    match location {
-        RVMLocation::GLOBAL => holder.instructions.push(Some(Instruction::FrameAlloc(amount, RegisterLocation::Global))),
-        RVMLocation::LOCAL => holder.instructions.push(Some(Instruction::FrameAlloc(amount, RegisterLocation::Local))),
-    }
+    holder.instructions.push(Some(Instruction::FrameAlloc(amount, real_loc(location))));
 }
 
 pub fn generate_frame_free(holder: &mut CodeHolder, amount: u32, location: RVMLocation) {
-    match location {
-        RVMLocation::GLOBAL => holder.instructions.push(Some(Instruction::FrameFree(amount, RegisterLocation::Global))),
-        RVMLocation::LOCAL => holder.instructions.push(Some(Instruction::FrameFree(amount, RegisterLocation::Local))),
-    }
+   holder.instructions.push(Some(Instruction::FrameFree(amount, real_loc(location)))); 
 }
 
 pub fn generate_jump(holder: &mut CodeHolder, instructions: i64) {
     holder.instructions.push(Some(Instruction::Jump(instructions)));
 }
 
-pub fn generate_call(holder: &mut CodeHolder, loc: u32) {
+pub fn generate_call(holder: &mut CodeHolder, loc: u64) {
     holder.instructions.push(Some(Instruction::Call(loc)));
 }
 
-pub fn generate_ext_call(holder: &mut CodeHolder, instruction_id: u32) {
+pub fn generate_ext_call(holder: &mut CodeHolder, instruction_id: u64) {
     holder.instructions.push(Some(Instruction::ExtCall(instruction_id)));
 }
 
 pub fn generate_return(holder: &mut CodeHolder) {
     holder.instructions.push(Some(Instruction::Ret));
+}
+
+pub fn generate_mov(holder: &mut CodeHolder, register_1: (RVMRegister, RVMReference), register_2: (RVMRegister, RVMReference)) {
+    let reference_1 = real_reference(register_1.1);
+    let reference_2 = real_reference(register_2.1);
+    let real_reg_1 = real_register(register_1.0);
+    let real_reg_2 = real_register(register_2.0);
+    holder.instructions.push(Some(Instruction::Mov(real_reg_1, reference_1, real_reg_2, reference_2)));
 }
